bf508feecf8d82696d6d55bc6b23d6689723e0ce
Ed <edadeagbo@gmail.com>
2022-03-27 22:27:12 -0600

    added files

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <BaseCard />
+    <BaseCardImage :cardInfo="cardInfo" :img="CardImage" />
+  </div>
+  <div>
+    <BaseCard />
+    <BaseCardImage :cardInfo="cardInfo" :img="CardImage" />
+  </div>
+  <div>
+    <BaseCard />
+    <BaseCardImage :cardInfo="cardInfo" :img="CardImage" />
+  </div>
+</template>
+
+<script setup>
+import BaseCard from "./components/BaseCard.vue";
+import BaseCardImage from "./components/BaseCardImage.vue";
+
+const cardInfo = {
+  cardTitle: "NEW ARRIVALS",
+};
+const CardImage = {
+  url: "https://empireave.com/wp-content/uploads/2020/02/Stussy-SP20-Lookbook-D2-Mens-17.jpg",
+  alt: "One new product from stussy",
+};
+</script>
